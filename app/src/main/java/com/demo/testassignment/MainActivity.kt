1fe99128ac4f02563ac9444b83de06b894871fe1
Poojak.ics <Poojak.ics@gmail.com>
2021-05-08 13:38:24 +0530

    Fragment created and dependency added

--- app/src/main/java/com/demo/testassignment/MainActivity.kt
+++ app/src/main/java/com/demo/testassignment/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.demo.testassignment
 
+import android.graphics.Color
 import android.os.Bundle
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
@@ -8,6 +9,8 @@ import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import com.demo.testassignment.ui.main.BookApiFragment
+import com.demo.testassignment.ui.main.NewsApiFragment
 import com.demo.testassignment.ui.main.SectionsPagerAdapter
 
 class MainActivity : AppCompatActivity() {
@@ -15,10 +18,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
+        val sectionsPagerAdapter = SectionsPagerAdapter(supportFragmentManager)
+        sectionsPagerAdapter.addFragment(NewsApiFragment(), "News")
+        sectionsPagerAdapter.addFragment(BookApiFragment(), "Book")
         val viewPager: ViewPager = findViewById(R.id.view_pager)
         viewPager.adapter = sectionsPagerAdapter
         val tabs: TabLayout = findViewById(R.id.tabs)
+        tabs.setTabTextColors(Color.GRAY,Color.WHITE); // set the tab text colors for the both states of the tab.
+
         tabs.setupWithViewPager(viewPager)
 
     }
--- app/src/main/java/com/demo/testassignment/ui/main/TabPagerAdapter.kt
+++ app/src/main/java/com/demo/testassignment/ui/main/TabPagerAdapter.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
+import androidx.fragment.app.FragmentStatePagerAdapter
 import com.demo.testassignment.R
 
 private val TAB_TITLES = arrayOf(
@@ -15,6 +16,7 @@ private val TAB_TITLES = arrayOf(
  * A [FragmentPagerAdapter] that returns a fragment corresponding to
  * one of the sections/tabs/pages.
  */
+/*
 class SectionsPagerAdapter(private val context: Context, fm: FragmentManager)
     : FragmentPagerAdapter(fm) {
 
@@ -31,5 +33,27 @@ class SectionsPagerAdapter(private val context: Context, fm: FragmentManager)
     override fun getCount(): Int {
         // Show 2 total pages.
         return 2
+    }
+}*/
+class SectionsPagerAdapter(supportFragmentManager: FragmentManager) : FragmentStatePagerAdapter(supportFragmentManager) {
+
+    private val mFragmentList = ArrayList<Fragment>()
+    private val mFragmentTitleList = ArrayList<String>()
+
+    override fun getItem(position: Int): Fragment {
+        return mFragmentList.get(position)
+    }
+
+    override fun getCount(): Int {
+        return mFragmentList.size
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return mFragmentTitleList[position]
+    }
+
+    fun addFragment(fragment: Fragment, title: String) {
+        mFragmentList.add(fragment)
+        mFragmentTitleList.add(title)
     }
 }
